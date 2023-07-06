9bb203a0eb24675c05b491cc637e1f491256330c
Alexey <Lehan347@yandex.ru>
2023-07-06 23:11:14 +0300

    The program was written for task 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.Write("Введите первое число: ");
+int n1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n2 = int.Parse(Console.ReadLine());
+if (n1>n2)
+    Console.WriteLine($"max = {n1}, min = {n2}");
+else
+    Console.WriteLine($"max = {n2}, min = {n1}");
+    
